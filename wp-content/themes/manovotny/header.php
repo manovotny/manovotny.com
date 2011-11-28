72178fe5984e2321c4a93b99cd11dbe5ec2a9de7
Michael Novotny <manovotny@gmail.com>
2011-11-27 21:26:38 -0600

    [#7] Modify meta description

--- wp-content/themes/manovotny/header.php
+++ wp-content/themes/manovotny/header.php
@@ -3,7 +3,7 @@
 	<head> 
 		<meta charset="utf-8" /> 
 		<meta name="author" content="Michael Novotny"> 
-		<meta name="description" content="Personal landing page for software engineer and web developer Michael Novotny" /> 
+		<meta name="description" content="Personal landing page for software engineer and web developer Michael Novotny, located in West Des Moines, Iowa" /> 
 		<title>Michael Novotny | Software Engineer &amp; Web Developer</title>
 		<?php google_analytics( 'UA-27106984-1' ); ?>
 		<?php wp_head(); ?>
