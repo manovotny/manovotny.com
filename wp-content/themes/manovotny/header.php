956b3e21ce16c65f7b42722687cda710be88240e
Michael Novotny <manovotny@gmail.com>
2011-11-27 21:30:53 -0600

    [#8] Add meta keywords

--- wp-content/themes/manovotny/header.php
+++ wp-content/themes/manovotny/header.php
@@ -3,7 +3,8 @@
 	<head> 
 		<meta charset="utf-8" /> 
 		<meta name="author" content="Michael Novotny"> 
-		<meta name="description" content="Personal landing page for software engineer and web developer Michael Novotny, located in West Des Moines, Iowa" /> 
+		<meta name="description" content="Personal landing page for software engineer and web developer Michael Novotny." /> 
+		<meta name="keywords" content="Michael Novotny, Michael, Novotny, software engineer, software, engineer, web developer, web, developer, development, West Des Moines, Iowa" /> 
 		<title>Michael Novotny | Software Engineer &amp; Web Developer</title>
 		<?php google_analytics( 'UA-27106984-1' ); ?>
 		<?php wp_head(); ?>
