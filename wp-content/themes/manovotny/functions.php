aaefbc42db71df66417a70f960ea0a8b736e5575
Michael Novotny <manovotny@gmail.com>
2012-10-10 21:05:24 -0500

    Makes manovotny a child theme of Standard.

--- wp-content/themes/manovotny/functions.php
+++ wp-content/themes/manovotny/functions.php
@@ -1,118 +1,26 @@
 <?php
 
 
-
-//--------------------------------------------------------------------------------
-//  CORE FUNCTIONS
-//--------------------------------------------------------------------------------
-
-//require_once( get_template_directory() . '/core/analytics.php' );
-//require_once( get_template_directory() . '/core/css_browser_selector.php' );
-//require_once( get_template_directory() . '/core/core.php' );
-//require_once( get_template_directory() . '/core/get_the_image.php' );
-//require_once( get_template_directory() . '/core/wordpress.php' );
-
-
-
-//--------------------------------------------------------------------------------
-//  WORDPRESS HOOKS
-//--------------------------------------------------------------------------------
-
+/* WordPress Hooks
+---------------------------------------------------------------------------------- */
 
 /**
- * Enqueueing scripts.
- */
-
-function man_enqueue_scripts()
-{	
-	// jquery
-	wp_enqueue_script( 'jquery' );
-	
-	// tipsy
-	wp_register_script( 'tipsy-script', get_template_directory_uri() . '/lib/tipsy/jquery.tipsy.js', false, false, true );
-	wp_enqueue_script( 'tipsy-script' );
-	
-	// theme
-	wp_register_script( 'theme-script', get_template_directory_uri() . '/js/theme.js', false, false, true );
-	wp_enqueue_script( 'theme-script' );
-}
-
-add_action( 'wp_enqueue_scripts', 'man_enqueue_scripts' );
-
-
-
-/**
- * Loads header items (ie. favicon, meta tags, link tags, etc.).
+ * Enqueueing scripts and styles.
  */
  
-function man_head()
-{
-	_e( '<link rel="shortcut icon" href="' . man_get_image( 'favicon.png', true ) . '" />' . "\n" );
-}
-
-add_action( 'wp_head', 'man_head' );
-
-
-
-/**
- * Enqueueing styles.
- */
- 
-function man_print_styles() 
-{
-	// wordpress
-	wp_register_style( 'wordpress-styles', get_template_directory_uri() . 'style.css' );
-	wp_enqueue_style( 'wordpress-styles' );
+function man_enqueue_scripts() {
 	
-	// theme
-	wp_register_style( 'theme-styles', get_template_directory_uri() . '/css/theme.css' );
-	wp_enqueue_style( 'theme-styles' );
+	// Standard.
+	wp_register_style( 'standard-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'standard-style' );
 	
-	// tipsy
-	wp_register_style( 'tipsy-styles', get_template_directory_uri() . '/lib/tipsy/tipsy.css' );
-	wp_enqueue_style( 'tipsy-styles' );
-}
+	// Theme.
+	wp_register_style( 'theme-style', get_stylesheet_directory_uri() . '/css/theme.css');
+	wp_enqueue_style( 'theme-style' );
 
-add_action( 'wp_print_styles', 'man_print_styles' );
+} // end man_enqueue_scripts
 
-
-
-//--------------------------------------------------------------------------------
-//  THEME FUNCTIONS
-//--------------------------------------------------------------------------------
-
-
-/**
- * Convenience function to get theme images.
- */
-
-function man_get_image( $image, $return = false, $sub_dirs = '' ) 
-{
-	// set image url
-	$image_url = get_template_directory_uri() . '/images/' . $sub_dirs . $image;
-	
-	// check return
-	if ( $return )
-	{
-		// return image url
-		return $image_url;
-	}
-
-	// echo image url
-	_e( $image_url );
-}
-
-
-
-/**
- * Renders copyright.
- */
- 
-function man_copyright()
-{
-	_e( 'Copyright &copy;' . ' ' . date( 'Y' ) . ' ' . get_bloginfo() . '. All rights reserved.' );
-}
-
+add_action( 'wp_enqueue_scripts', 'man_enqueue_scripts', 10000 );
 
 
 ?>
---
+++ wp-content/themes/manovotny/template-about.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: About
+ * 
+ * The template for home page.
+ *
+ * @package manovotny
+ * @since 2.0
+ */
+?>
+<?php get_header(); ?>
+
+<div id="wrapper">
+	<div class="container">
+		<div class="row">
+
+			<div id="main" class="span12 clearfix" role="main">
+		
+					<h3 class="title standout">Hello, I am Michael.</h3>
+					<h4 class="tagline standout">I am a developer. I make things.</h4>
+					<p>I am a software engineer for <a href="http://webfilings.com/" title="WebFilings" target="_blank">WebFilings</a>, I lead quality assurance for <a href="http://8bit.io/" title="8BIT" target="_blank">8BIT</a>, and I do freelance development for fun, like <a href="http://iowagirleats.com/" title="Iowa Girl Eats" target="_blank">Iowa Girl Eats</a>.</p>
+					<p>I am obsessed with code. I never stop learning.</p>
+					<p>Drop me a line if you would like to <a href="mailto:manovotny@gmail.com" title="Email">talk</a>.</p>
+		
+			</div> <!-- /#main -->
+		
+		</div> <!-- /row -->
+	</div> <!-- /container -->
+</div> <!-- /#wrapper -->
+<?php get_footer(); ?>
