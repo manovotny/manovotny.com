48850a0bff6f48c23b8f2a6c44d5cbf4f0a9b086
Michael Novotny <manovotny@manovotny-rmbp.local>
2014-03-11 22:16:31 -0500

    Adds custom Mayer child theme.

---
+++ wp-content/themes/mayer-child/partials/avatar.php
@@ -0,0 +1,5 @@
+<div id="avatar" class="col-lg-2 col-md-2 col-sm-12 hidden-xs">
+    <a href="<?php echo esc_url( home_url() ); ?>" title="<?php bloginfo( 'title' ); ?>">
+        <img src="<?php echo get_stylesheet_directory_uri() . '/images/logo.jpg' ?>" alt="<?php get_bloginfo( 'title' ) ?>" />
+    </a>
+</div><!-- #avatar -->
