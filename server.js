447909c126888f43fb2e9dec26bd09455f2b4ed8
Michael Novotny <manovotny@gmail.com>
2018-04-10 16:45:02 -0500

    Changes server to use dynamically available port.

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-const express = require('express')
-const next = require('next')
+const getPort = require('get-port');
+const express = require('express');
+const next = require('next');
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev});
+const handle = app.getRequestHandler();
 
 app.prepare()
-.then(() => {
-  const server = express()
+    .then(async () => {
+        const server = express();
+        const port = await getPort({port: 3000});
 
-  // custom route for posts
-  server.get('/post/:id', (req, res) => {
-    return app.render(req, res, '/post', {
-      id: req.params.id
-    })
-  })
+        server.get('/post/:id', (request, response) =>
+            app.render(request, response, '/post', {
+                id: request.params.id
+            })
+        );
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+        server.get('*', (request, response) =>
+            handle(request, response)
+        );
 
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+        server.listen(port, (error) => {
+            if (error) {
+                throw error;
+            }
+
+            console.log(`> Ready on http://localhost:${port}`)
+        })
+    });
