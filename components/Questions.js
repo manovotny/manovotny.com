2f590fdc45a81622e2a91a43d94444583ec06e04
Michael Novotny <manovotny@gmail.com>
2018-04-17 18:21:13 -0500

    Fixes typo in questions section.

--- components/Questions.js
+++ components/Questions.js
@@ -11,7 +11,7 @@ const Questions = () =>
     <Section>
         <h2>Questions?</h2>
         <p>Have a question about this post or anything else?</p>
-        <p>Ask away on <a href="https://twitter.com/manovotny" title="Twitter">Twitter</a> or in my <a href="https://github.com/manovotny/ama" title="Ask Me Anything">GitHub AMA</a>!</p>
+        <p>Ask away on <a href="https://twitter.com/manovotny" title="Twitter">Twitter</a> or on my <a href="https://github.com/manovotny/ama" title="Ask Me Anything">GitHub AMA</a>!</p>
     </Section>;
 
 export default Questions;
