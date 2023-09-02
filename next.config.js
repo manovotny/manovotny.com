a17938996b43c7ececabb334811f2367476f7a61
Michael Novotny <manovotny@gmail.com>
2023-09-02 00:18:42 -0500

    Ignore eslint during builds

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        // Disabling on production builds because we're running checks on PRs via GitHub Actions.
+        ignoreDuringBuilds: true,
+    },
     experimental: {
         mdxRs: true,
         serverActions: true,
