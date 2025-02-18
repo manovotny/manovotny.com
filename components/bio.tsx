d4626dce61c35f65df756135d293e99f1ae29e57
Michael Novotny <manovotny@gmail.com>
2025-02-17 19:58:59 -0600

    Fix bio dark mode

--- components/bio.tsx
+++ components/bio.tsx
@@ -4,7 +4,7 @@ export function Bio() {
   return (
     <h1 className="text-2xl">
       <span className="font-bold">Michael Novotny</span>
-      <span className="font-extralight text-neutral-500">
+      <span className="font-extralight text-neutral-500 dark:text-neutral-400">
         —software developer, stock trader, coffee enthusiast. Currently leading
         Content at{" "}
         <Link href="https://vercel.com" target="_blank">
