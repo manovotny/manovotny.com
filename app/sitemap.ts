377d99c583a6c67c9a0c110f50a70fe595557285
Michael Novotny <manovotny@gmail.com>
2025-05-03 14:05:54 -0400

    Simplifies sitemap even further

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,26 +2,23 @@ import { promises as fs } from "node:fs";
 import path from "node:path";
 import { MetadataRoute } from "next";
 
-async function getPages(dir: string) {
+async function getRoutes(dir: string) {
   const entries = await fs.readdir(dir, {
     recursive: true,
     withFileTypes: true,
   });
   return entries
     .filter((entry) => entry.isFile() && entry.name === "page.mdx")
-    .map((entry) =>
-      entry.parentPath.replace(dir, "").replace(/\/\([^\/]*\)/g, ""),
-    );
+    .map((entry) => ({
+      // Replace the parent directory and remove group routes
+      url: `https://manovotny.com${entry.parentPath.replace(dir, "").replace(/\/\([^\/]*\)/g, "")}`,
+      lastModified: new Date().toISOString(),
+    }));
 }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const appDirectory = path.join(process.cwd(), "app");
-  const pages = await getPages(appDirectory);
-
-  const routes = pages.map((page) => ({
-    url: `https://manovotny.com${page}`,
-    lastModified: new Date().toISOString(),
-  }));
+  const routes = await getRoutes(appDirectory);
   const sitemap = [...routes];
 
   return sitemap;
