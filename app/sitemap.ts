13557bde87815dbac767e8431087c92d29715ca2
Michael Novotny <manovotny@gmail.com>
2025-05-03 14:01:24 -0400

    Simplified sitemap logic

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,34 +2,27 @@ import { promises as fs } from "node:fs";
 import path from "node:path";
 import { MetadataRoute } from "next";
 
-async function getNoteSlugs(dir: string) {
+async function getPages(dir: string) {
   const entries = await fs.readdir(dir, {
     recursive: true,
     withFileTypes: true,
   });
   return entries
     .filter((entry) => entry.isFile() && entry.name === "page.mdx")
-    .map((entry) => {
-      const relativePath = path.relative(
-        dir,
-        path.join(entry.parentPath, entry.name),
-      );
-      return path.dirname(relativePath).replace(/\\/g, "/");
-    });
+    .map((entry) =>
+      entry.parentPath.replace(dir, "").replace(/\/\([^\/]*\)/g, ""),
+    );
 }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const notesDirectory = path.join(process.cwd(), "app", "(notes)");
-  const slugs = await getNoteSlugs(notesDirectory);
-  const notes = slugs.map((slug) => ({
-    url: `https://manovonty.com/${slug}`,
+  const appDirectory = path.join(process.cwd(), "app");
+  const pages = await getPages(appDirectory);
+
+  const routes = pages.map((page) => ({
+    url: `https://manovotny.com${page}`,
     lastModified: new Date().toISOString(),
   }));
-  const routes = [""].map((route) => ({
-    url: `https://manovotny.com${route}`,
-    lastModified: new Date().toISOString(),
-  }));
-  const sitemap = [...routes, ...notes];
+  const sitemap = [...routes];
 
   return sitemap;
 }
