f1d700161ffbf86d25295d4be50eaa957766a286
Michael Novotny <manovotny@gmail.com>
2025-05-03 13:43:35 -0400

    Adds custom font to OG image

--- app/api/og-image/route.tsx
+++ app/api/og-image/route.tsx
@@ -1,40 +1,74 @@
 import { ImageResponse } from "next/og";
 import { Logo } from "@/components/icons/logo-dot";
 
-export const size = {
-  width: 1200,
-  height: 630,
-};
+// https://developers.google.com/fonts/docs/css2
+async function loadGoogleFont(font: string, weight: number, text: string) {
+  const url = `https://fonts.googleapis.com/css2?family=${font.replaceAll(" ", "+")}:wght@${weight}&text=${encodeURIComponent(text)}`;
+  const css = await (await fetch(url)).text();
+  const resource = css.match(
+    /src: url\((.+)\) format\('(opentype|truetype)'\)/,
+  );
+
+  if (resource) {
+    const fontUrl = resource[1];
+    const response = await fetch(fontUrl);
+
+    if (response.status == 200) {
+      return await response.arrayBuffer();
+    }
+  }
+
+  throw new Error(`Failed to load font data: ${font}@${weight}`);
+}
 
 export async function GET(request: Request) {
   try {
+    const domain = "manovotny.com";
     const { searchParams } = new URL(request.url);
     const title = searchParams.has("title")
-      ? searchParams.get("title")
+      ? searchParams.get("title") || "Michael Novotny"
       : "Michael Novotny";
 
     return new ImageResponse(
       (
-        <div tw="bg-neutral-50 text-neutral-800 flex h-full w-full flex-col p-8">
+        <div tw="bg-neutral-50 text-neutral-800 flex font-['Geist'] h-full w-full flex-col p-8">
           <div tw="flex flex-row justify-between">
             <Logo size={48} />
-            <p tw="m-0 p-0 text-5xl font-light">manovotny.com</p>
+            <p tw="m-0 p-0 text-5xl font-thin">{domain}</p>
           </div>
           <p
-            tw="text-center text-7xl font-bold m-auto"
-            style={{ textWrap: "balance" }}
+            tw="text-center text-7xl m-auto font-bold"
+            style={{
+              textWrap: "balance",
+            }}
           >
             {title}
           </p>
         </div>
       ),
       {
-        ...size,
+        width: 1200,
+        height: 630,
+        // https://fonts.google.com/specimen/Geist
+        fonts: [
+          {
+            name: "Geist",
+            data: await loadGoogleFont("Geist", 100, domain),
+            style: "normal",
+            weight: 100,
+          },
+          {
+            name: "Geist",
+            data: await loadGoogleFont("Geist", 700, title),
+            style: "normal",
+            weight: 700,
+          },
+        ],
       },
     );
   } catch (e: any) {
     console.log(`${e.message}`);
-    return new Response(`Failed to generate the image`, {
+    return new Response("Failed to generate og image", {
       status: 500,
     });
   }
