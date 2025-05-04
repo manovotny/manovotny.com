d1517b453ca891e1942e7f37a5ff2f61f7b45849
Michael Novotny <manovotny@gmail.com>
2025-05-04 09:59:00 -0400

    Adds last modified date to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,19 +1,30 @@
 import { promises as fs } from "node:fs";
 import path from "node:path";
 import { MetadataRoute } from "next";
+import { getLastModifiedDate } from "git-jiggy";
 
 async function getRoutes(dir: string) {
   const entries = await fs.readdir(dir, {
     recursive: true,
     withFileTypes: true,
   });
-  return entries
-    .filter((entry) => entry.isFile() && entry.name === "page.mdx")
-    .map((entry) => ({
-      // Replace the parent directory and remove group routes
-      url: `https://manovotny.com${entry.parentPath.replace(dir, "").replace(/\/\([^\/]*\)/g, "")}`,
-      lastModified: new Date().toISOString(),
-    }));
+  const routes = [];
+
+  for (const entry of entries) {
+    if (entry.isFile() && entry.name === "page.mdx") {
+      const lastModified = await getLastModifiedDate(
+        `${entry.parentPath}/${entry.name}`,
+      );
+
+      routes.push({
+        // Replace the parent directory and remove group routes
+        url: `https://manovotny.com${entry.parentPath.replace(dir, "").replace(/\/\([^\/]*\)/g, "")}`,
+        lastModified,
+      });
+    }
+  }
+
+  return routes;
 }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
