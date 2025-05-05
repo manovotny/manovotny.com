4edb186e23c211001c616a7714452178a95c3f4c
Michael Novotny <manovotny@gmail.com>
2025-05-05 16:16:13 -0400

    Conditionally renders name in header (ie. not showing on home page)

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { Link } from "@/components/link";
 import { Logo } from "@/components/icons/logo";
 
 export function Header() {
+  const pathname = usePathname();
+  const isHome = pathname === "/";
+
   return (
     <header className="flex py-16">
       <Link
@@ -9,14 +15,16 @@ export function Header() {
         href="/"
       >
         <Logo />
-        <p className="mt-[-4px] h-[28px] transform text-2xl font-light transition-transform duration-[400ms] group-hover:translate-y-[-33px]">
-          <span className="block origin-[right_center] transform transition-transform duration-[400ms]">
-            Michael Novotny
-          </span>
-          <span className="block origin-[left_center] transform transition-transform duration-[400ms]">
-            manovotny
-          </span>
-        </p>
+        {isHome ? null : (
+          <p className="mt-[-4px] h-[28px] transform text-2xl font-light transition-transform duration-[400ms] group-hover:translate-y-[-33px]">
+            <span className="block origin-[right_center] transform transition-transform duration-[400ms]">
+              Michael Novotny
+            </span>
+            <span className="block origin-[left_center] transform transition-transform duration-[400ms]">
+              manovotny
+            </span>
+          </p>
+        )}
       </Link>
       {/* <nav className="ml-auto">
         <ol className="flex gap-10">
