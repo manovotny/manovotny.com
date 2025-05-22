e66b5a06263ce045e514c60351d5bf45b8950086
Michael Novotny <manovotny@gmail.com>
2025-05-21 23:04:07 -0500

    Updates bio to have H1 and H2

--- components/bio.tsx
+++ components/bio.tsx
@@ -2,13 +2,13 @@ import { Link } from "@/components/link";
 
 export function Bio() {
   return (
-    <h1 className="text-2xl text-balance">
-      <span className="font-bold">Michael Novotny</span>
-      <span className="font-extralight text-neutral-500 dark:text-neutral-400">
-        —software developer, stock investor/trader, avid coffee drinker.
+    <div className="text-2xl text-balance">
+      <h1 className="inline font-bold">Michael Novotny</h1>
+      <h2 className="dark:text-neutral-40 inline font-extralight text-neutral-500 before:content-['—']">
+        software developer, stock investor/trader, avid coffee drinker.
         Currently leading content at{" "}
         <Link href="https://vercel.com/home">Vercel</Link>.
-      </span>
-    </h1>
+      </h2>
+    </div>
   );
 }
