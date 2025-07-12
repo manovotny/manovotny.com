bb0aba3d45ae3c9ca6bdcb0f1596c0927f26ebf0
Michael Novotny <manovotny@gmail.com>
2025-07-12 16:24:42 -0500

    Back to Vercel system env vars

--- lib/environment.ts
+++ lib/environment.ts
@@ -1,6 +1,6 @@
 export const baseUrl =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
-    : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
+  process.env.VERCEL_ENV === "production"
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : process.env.VERCEL_ENV === "preview"
+      ? `https://${process.env.VERCEL_BRANCH_URL}`
       : "http://localhost:3000";
